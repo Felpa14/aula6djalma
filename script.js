064e895fef395912216ccc5cd96d56cbd156d389
Felipe Gonçalves <105553680+Felpa14@users.noreply.github.com>
2022-09-21 21:37:08 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function somar(){
+
+    var valor1 = document.getElementById("Valor1").value;
+    var valor2 = document.getElementById("Valor2").value;
+    var somar = valor1 + valor2;
+    alert(somar);
+}
